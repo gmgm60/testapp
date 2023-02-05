36cdc7f9461bb889ed6b31c2c08ac063fe42c954
Mohamed Gamal <mohamedgamalma3011@gmail.com>
2023-02-05 21:59:35 +0200

    edit text

--- lib/main.dart
+++ lib/main.dart
@@ -70,7 +70,7 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
+              'Last Picked File Size:',
             ),
             if(kIsWeb)
             Text(
